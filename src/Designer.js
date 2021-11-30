b2065c6d64aa433126291db0d495fabedbcdab21
Your Name <you@example.com>
2021-11-30 21:02:14 +1100

    layout for design pages

---
+++ src/Designer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Designer() {
+    return (
+        <div>
+            {/* Product Name*/}
+            {/* Delivery Date*/}
+            {/* Range*/} 
+            {/* Speed*/}
+            {/* 0-100 time*/}
+            <ul>
+                <li>Range (est.)</li>
+                <li>Top Speed</li>
+                <li>0-100 km/h</li>
+            </ul>
+            {/* Feature Name*/}
+            {/* Input Box for feature*/}
+            
+            {/* Feature Name*/}
+            {/* Input Box for feature*/}
+        </div>
+    )
+}
+
+export default Designer
