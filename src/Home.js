d68d195eecc5d85a226f79959392d0e7c3b4c1c6
Your Name <you@example.com>
2021-11-30 21:01:58 +1100

    converted homepage to single component

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import BodyItem from './BodyItem';
+import Footer from './Footer';
+import Header from "./Header.js"
+
+function Home() {
+    return (
+        <div className="home">
+            <Header />
+            <BodyItem product="Model 3" description="Electric vehicle incentives are now available on eligible Model 3 in ACT, NSW, TAS and VIC. Learn More" first__btn="EXISTING INVENTORY" second__btn="CUSTOM ORDER" background__image="https://tesla-cdn.thron.com/delivery/public/image/tesla/ca583b8c-7cfa-4202-9be2-1d3f0e3f24cf/bvlatuR/std/2880x1800/M3-Homepage-RHD-D"/>
+            <BodyItem product="Solar and Powerwall" description="Power Everything" first__btn="LEARN MORE" second__btn="STAY UPDATED" background__image="https://tesla-cdn.thron.com/delivery/public/image/tesla/45992f1c-a33a-4a04-b1f0-338aff182f8e/bvlatuR/std/2880x1800/_25-Hero-D"/>
+            <BodyItem product="Model Y" first__btn="LEARN MORE" second__btn="STAY UPDATED" background__image="https://tesla-cdn.thron.com/delivery/public/image/tesla/8e2df1b9-a4bf-4eb9-beec-2cf5cc77fca0/bvlatuR/std/2880x2400/Desktop-ModelY?quality=70"/>
+            <BodyItem product="Model S" description="Schedule a Touchless Test Drive" first__btn="CUSTOM ORDER" second__btn="LEARN MORE" background__image="https://tesla-cdn.thron.com/delivery/public/image/tesla/32e5e0f3-5c04-42ef-8f8f-c6b1c26f8a9e/bvlatuR/std/2880x1800/ms-main-hero-desktop"/>
+            <BodyItem product="Model X" description="Schedule a Touchless Test Drive" first__btn="CUSTOM ORDER" second__btn="LEARN MORE" background__image="https://tesla-cdn.thron.com/delivery/public/image/tesla/325743a2-d187-4e28-91a3-5a2e47e317f7/bvlatuR/std/2880x1800/ModelX-Homepage-D"/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Home
