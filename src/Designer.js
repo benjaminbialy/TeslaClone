62666a5baae8312fd27a147701f8ff6f94aaab08
Your Name <you@example.com>
2021-12-01 20:23:03 +1100

    started developing template for custom order

--- src/Designer.js
+++ src/Designer.js
@@ -1,23 +1,27 @@
 import React from 'react'
-
-function Designer() {
+import "./Designer.css"
+function Designer(props) {
     return (
-        <div>
-            {/* Product Name*/}
-            {/* Delivery Date*/}
-            {/* Range*/} 
-            {/* Speed*/}
-            {/* 0-100 time*/}
-            <ul>
-                <li>Range (est.)</li>
-                <li>Top Speed</li>
-                <li>0-100 km/h</li>
-            </ul>
-            {/* Feature Name*/}
-            {/* Input Box for feature*/}
-            
-            {/* Feature Name*/}
-            {/* Input Box for feature*/}
+        <div className="Designer">
+            <div className="designer--column">
+                <h1 className="designer--column--product">{props.product}</h1>
+                <p className="designer--column--delivery__date">{props.delivery__date}</p>
+                <div className="designer--column--stats">
+                    <div className="designer--column--stats--range">{props.range}</div>
+                    <div className="designer--column--stats--speed">{props.speed}</div>
+                    <div className="designer--column--stats--zero__to__hundred">{props.time}</div>
+                    <div className="designer--column--stats--range__label">Range (est.)</div>
+                    <div className="designer--column--stats--speed__label">Top Speed</div>
+                    <div className="designer--column--stats--zero__to__hundred__label">0-100 km/h</div>
+                </div>
+                <h2 className="designer--column--feature__label">Dual Motor All-Wheel Drive</h2>
+                <div className="designer--column--select">
+                    .designer--column--select
+                </div>
+                
+                {/* Feature Name*/}
+                {/* Input Box for feature*/}
+            </div>
         </div>
     )
 }
